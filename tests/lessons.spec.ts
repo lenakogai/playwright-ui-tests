07a827102bb44238df1aac00dda6a00720ccc97d
Elena Kogai <kogai.lena@gmail.com>
2022-08-28 00:44:24 +0500

    добавила тесты из урока в lessons

--- tests/lessons.spec.ts
+++ tests/lessons.spec.ts
@@ -1,17 +1,70 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, _baseTest } from '@playwright/test';
 
-// Урок 1
+// Урок 3
 
-/*
-test('Тестовый запуск1', async({page})=>{
+test('Текстовый селектор', async({page})=>{
+
     await page.goto('https://jpetstore.aspectran.com/catalog/')
     await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
-    const locator = page.locator('[id="Search"]')
-    await expect(locator).toBeVisible()
-    await locator.click()
-    await locator.type('Давай мясо!')
-    const search = page.locator('[class="input-group-button"]').last()
-    await expect(search).toBeVisible()
-    await expect(locator).toHaveText('Search')
+    const signIn = page.locator('text=Sign In')
+    await expect(signIn).toBeVisible()
+    await expect(signIn).toHaveText('Sign In')
 })
-*/
+
+test('CSS селектор с id', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const logo = page.locator('#Logo')
+    await expect(logo).toBeVisible()
+})
+
+test('CSS селектор с class', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const logoClass = page.locator('.grid-container').nth(4)
+    await expect(logoClass).toBeVisible()
+})
+
+test('CSS селектор-атрибут', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const masthead = page.locator('[id="masthead"]')
+    await expect(masthead).toBeVisible()
+})
+
+test('Вложенный элемент. Фильтрация по тексту', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const search = page.locator('.input-group-button', {hasText:'Search'})
+    await expect(search).toBeVisible()
+})
+
+test('Вложенный элемент. Фильтрация по другому локатору', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const placeholder = page.locator('#Search', {has: page.locator('[placeholder="Product Search"]')})
+    await expect(placeholder).toBeVisible()
+})
+
+test('Вложенный элемент. Обращение к дочернему элементу', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const searchToClass = page.locator('[id="Search"]>>[class="input-group-field"]')
+    await expect(searchToClass).toBeVisible()
+})
+
+test.only('Вложенный элемент. Проверка наличия атрибутов через функцию toHaveAttribute()', async({page})=>{
+
+    await page.goto('https://jpetstore.aspectran.com/catalog/')
+    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+    const search = page.locator('[id="Search"]')
+    await expect (search).toBeVisible()
+    const searchToClass = page.locator('[id="Search"]>>[class="input-group-field"]')
+    await expect(searchToClass).toHaveAttribute('placeholder','Product Search')
+})
