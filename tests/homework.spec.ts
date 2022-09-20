e438efce62970bee1d781e446a4f2004acdc360b
Elena Kogai <kogai.lena@gmail.com>
2022-09-20 23:18:11 +0500

    Добавила beforeEach перед тестами урока 2

--- tests/homework.spec.ts
+++ tests/homework.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Урок 2: домашнее задание #1', () => {
+test.beforeEach( async({page})=>{    
+    await page.goto('https://jpetstore.aspectran.com/account/signonForm')
+})
+
+test.describe.only('Урок 2: домашнее задание #1', () => {
 
     // Задание 1
     test('Проверка url страницы авторизации', async({page})=>{
-    
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
     
     })
     
     // Задание 2
     test('Наличие элементов на странице', async({page})=>{
-    
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         const fieldUsername = page.locator('[name="username"]')
         const fieldPassword = page.locator('[name="password"]')
@@ -28,8 +28,6 @@ test.describe('Урок 2: домашнее задание #1', () => {
     
     // Задание 3
     test('Ввести валидные данные в поля username и password, нажать на Login → авторизация успешна', async({page})=>{
-    
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Очищаем поле и вводим username
         const fieldUsername = page.locator('[name="username"]')
@@ -56,8 +54,6 @@ test.describe('Урок 2: домашнее задание #1', () => {
     })
     
     test('Ввести невалидные данные в поля username и password, нажать на Login → ошибка, авторизация не успешна', async({page})=>{
-    
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Очищаем поле и вводим username
         const fieldUsername = page.locator('[name="username"]')
@@ -84,8 +80,7 @@ test.describe('Урок 2: домашнее задание #1', () => {
     })
     
     test('Ввести валидные данные в поле username, в password невалидные, нажать на Login → ошибка, авторизация не успешна', async({page})=>{
-    
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
+
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Очищаем поле и вводим username
         const fieldUsername = page.locator('[name="username"]')
@@ -113,7 +108,6 @@ test.describe('Урок 2: домашнее задание #1', () => {
     
     test('Ввести невалидные данные в поле username, в поле password валидные, нажать на Login → ошибка, авторизация не успешна', async({page})=>{
     
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Очищаем поле и вводим username
         const fieldUsername = page.locator('[name="username"]')
@@ -141,7 +135,6 @@ test.describe('Урок 2: домашнее задание #1', () => {
     
     test('Не заполнять поля username и password, нажать на Login → ошибка, авторизация не успешна', async({page})=>{
     
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Очищаем поле username
         const fieldUsername = page.locator('[name="username"]')
@@ -167,7 +160,6 @@ test.describe('Урок 2: домашнее задание #1', () => {
     
     test('Ввести валидные данные в поле username и не заполнять поле password, нажать на Login → ошибка, авторизация не успешна', async({page})=>{
     
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Вводим username
         const fieldUsername = page.locator('[name="username"]')
@@ -194,7 +186,6 @@ test.describe('Урок 2: домашнее задание #1', () => {
     
     test('Не заполнять поле username и ввести валидные данные в поле password, нажать на Login → ошибка, авторизация не успешна', async({page})=>{
     
-        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
         // Очищаем поле username
         const fieldUsername = page.locator('[name="username"]')
