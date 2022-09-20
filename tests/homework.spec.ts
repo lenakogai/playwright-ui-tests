75867554e6a904681ff49510ff7ed424bdb3e792
Elena Kogai <kogai.lena@gmail.com>
2022-09-21 00:42:02 +0500

    Выполнила 1 и 2 задания

--- tests/homework.spec.ts
+++ tests/homework.spec.ts
@@ -4,12 +4,11 @@ test.beforeEach( async({page})=>{
     await page.goto('https://jpetstore.aspectran.com/account/signonForm')
 })
 
-test.describe.only('Урок 2: домашнее задание #1', () => {
+test.describe('Урок 2: домашнее задание #1', () => {
 
     // Задание 1
     test('Проверка url страницы авторизации', async({page})=>{
         await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
-    
     })
     
     // Задание 2
@@ -21,8 +20,7 @@ test.describe.only('Урок 2: домашнее задание #1', () => {
         await expect(fieldUsername).toBeVisible()
         await expect(fieldPassword).toBeVisible()
         await expect(buttonLogin).toBeVisible()
-        await expect(panelRegister).toBeVisible()
-    
+        await expect(panelRegister).toBeVisible()  
     })
     
     // Задание 3
@@ -205,58 +203,120 @@ test.describe.only('Урок 2: домашнее задание #1', () => {
 
 test.describe('Урок 3: домашнее задание #2', () => {
 
-// Задание 1
-test('Селекторы: текст', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/catalog/')
-    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
-    // Селектор для элемента Fish
-    const fish = page.locator('text=Fish').nth(0)
-    await expect(fish).toBeVisible()
-    // Селектор для элемента Dogs
-    const dogs = page.locator('text=Dogs').nth(0)
-    await expect(dogs).toBeVisible()
-    // Селектор для элемента Reptiles
-    const reptiles = page.locator('text=Reptiles').nth(0)
-    await expect(reptiles).toBeVisible()
-    // Селектор для элемента Cats
-    const cats = page.locator('text=Cats').nth(0)
-    await expect(cats).toBeVisible()   
-    // Селектор для элемента Birds
-    const birds = page.locator('text=Birds').nth(0)
-    await expect(birds).toBeVisible() 
-})
+    // Задание 1
+    test('Селекторы: текст', async({page})=>{
+        await page.goto('https://jpetstore.aspectran.com/catalog/')
+        await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+        // Селектор для элемента Fish
+        const fish = page.locator('text=Fish').nth(0)
+        await expect(fish).toBeVisible()
+        // Селектор для элемента Dogs
+        const dogs = page.locator('text=Dogs').nth(0)
+        await expect(dogs).toBeVisible()
+        // Селектор для элемента Reptiles
+        const reptiles = page.locator('text=Reptiles').nth(0)
+        await expect(reptiles).toBeVisible()
+        // Селектор для элемента Cats
+        const cats = page.locator('text=Cats').nth(0)
+        await expect(cats).toBeVisible()   
+        // Селектор для элемента Birds
+        const birds = page.locator('text=Birds').nth(0)
+        await expect(birds).toBeVisible() 
+    })
 
-// Задание 2
-test('Селекторы: CSS ID', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/catalog/')
-    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
-    const navigation = page.locator('#QuickLinks')
-    await expect(navigation).toBeVisible()
-})
-test('Селекторы: CSS Class', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/catalog/')
-    await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
-    const navigationClass = page.locator('.grid-container').nth(2)
-    await expect(navigationClass).toBeVisible()
-})
+    // Задание 2
+    test('Селекторы: CSS ID', async({page})=>{
+        await page.goto('https://jpetstore.aspectran.com/catalog/')
+        await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+        const navigation = page.locator('#QuickLinks')
+        await expect(navigation).toBeVisible()
+    })
+    test('Селекторы: CSS Class', async({page})=>{
+        await page.goto('https://jpetstore.aspectran.com/catalog/')
+        await expect(page).toHaveURL('https://jpetstore.aspectran.com/catalog/')
+        const navigationClass = page.locator('.grid-container').nth(2)
+        await expect(navigationClass).toBeVisible()
+    })
 
-// Задание 3
-test('Фильтрация по тексту', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/account/signonForm')
-    await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
-    const loginButton = page.locator('.button-bar', {hasText:'Login'})
-    await expect(loginButton).toBeVisible()
-})
-test('Фильтрация по другому локатору', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/account/signonForm')
-    await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
-    const signonPanel = page.locator('#Signon', {has: page.locator('[class="panel"]')})
-    await expect(signonPanel).toBeVisible()
-})
-test('Обращение к дочернему элементу', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/account/signonForm')
-    await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
-    const headerMenu = page.locator('[id="Header"]>>[id="Menu"]')
-    await expect(headerMenu).toBeVisible()
-})
+    // Задание 3
+    test('Фильтрация по тексту', async({page})=>{
+        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
+        await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
+        const loginButton = page.locator('.button-bar', {hasText:'Login'})
+        await expect(loginButton).toBeVisible()
+    })
+    test('Фильтрация по другому локатору', async({page})=>{
+        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
+        await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
+        const signonPanel = page.locator('#Signon', {has: page.locator('[class="panel"]')})
+        await expect(signonPanel).toBeVisible()
+    })
+    test('Обращение к дочернему элементу', async({page})=>{
+        await page.goto('https://jpetstore.aspectran.com/account/signonForm')
+        await expect(page).toHaveURL('https://jpetstore.aspectran.com/account/signonForm')
+        const headerMenu = page.locator('[id="Header"]>>[id="Menu"]')
+        await expect(headerMenu).toBeVisible()
+    })
+    });
+
+
+    test.beforeEach( async({page})=>{    
+        // Очищаем поле и вводим username
+        const fieldUsername = page.locator('[name="username"]')
+        await expect(fieldUsername).toBeVisible()
+        await fieldUsername.click()
+        await fieldUsername.press('Control+A')
+        await fieldUsername.press('Backspace')
+        await fieldUsername.type('j2ee')
+        // Очищаем поле и вводим password
+        const fieldPassword = page.locator('[name="password"]')
+        await expect(fieldPassword).toBeVisible()
+        await fieldPassword.click()
+        await fieldPassword.press('Control+A')
+        await fieldPassword.press('Backspace')
+        await fieldPassword.type('j2ee')
+        // Нажимаем Login
+        const buttonLogin = page.locator('[class="button-bar"]').last()
+        await expect(buttonLogin).toBeVisible()
+        await buttonLogin.click()
+        // Проверяем успешность авторизации
+        const elementWelcome = page.locator('[id="WelcomeContent"]')
+        await expect(elementWelcome).toBeVisible()
+    })
+    
+test.describe('Урок 4: домашнее задание #5', () => {
+
+    // Задание 1
+
+    test('Проверка цветов элементов Pet Favorites', async({page})=>{  
+        const petFavoritesText = page.locator('text=Pet Favorites')
+        await expect(petFavoritesText).toHaveCSS('color','rgb(10, 10, 10)')
+        await expect(petFavoritesText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+        const shopText = page.locator('text=Shop for more of your favorite pets here.')
+        await expect(shopText).toHaveCSS('color','rgb(10, 10, 10)')
+        await expect(shopText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+        const manxText = page.locator('text=Manx')
+        await expect(manxText).toHaveCSS('color','rgb(0, 136, 204)')
+        await expect(manxText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+        const persianText = page.locator('text=Persian')
+        await expect(persianText).toHaveCSS('color','rgb(0, 136, 204)')
+        await expect(persianText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+    })
+
+    // Задание 2
+
+    test('Проверка цветов элементов Menu Content', async({page})=>{  
+        const myOrdersText = page.locator('text=My Orders')
+        await expect(myOrdersText).toHaveCSS('color','rgb(234, 172, 0)')
+        await expect(myOrdersText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+        const myAccountText = page.locator('text=My Account')
+        await expect(myAccountText).toHaveCSS('color','rgb(234, 172, 0)')
+        await expect(myAccountText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+        const mySignOutText = page.locator('text=Sign Out')
+        await expect(mySignOutText).toHaveCSS('color','rgb(234, 172, 0)')
+        await expect(mySignOutText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+        const questionMarkText = page.locator('text=?')
+        await expect(questionMarkText).toHaveCSS('color','rgb(234, 172, 0)')
+        await expect(questionMarkText).toHaveCSS('background-color','rgba(0, 0, 0, 0)')
+    })
 });
