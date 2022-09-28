7b1b8dd4ca00eea961d00a987fb3598dc4657b0f
Elena Kogai <kogai.lena@gmail.com>
2022-09-29 02:05:35 +0500

    Отрефакторила тесты 2 урока

--- pages/authPage.ts
+++ pages/authPage.ts
@@ -2,15 +2,21 @@ import { Locator, Page } from '@playwright/test';
 
 export class AuthPage {
     readonly page:Page
-    readonly username: Locator
-    readonly password: Locator
-    readonly loginButton: Locator
+    readonly fieldUsername: Locator
+    readonly fieldPassword: Locator
+    readonly buttonLogin: Locator
     readonly panelRegister: Locator
+    readonly loginText: Locator
+    readonly elementWelcome: Locator
+    readonly elementLoginFailed: Locator
     
     constructor(page:Page){
-        this.username = page.locator('[name="username"]')
-        this.password = page.locator('[name="password"]')
-        this.loginButton = page.locator('text=Login')
+        this.fieldUsername = page.locator('[name="username"]')
+        this.fieldPassword = page.locator('[name="password"]')
+        this.buttonLogin = page.locator('[class="button-bar"]').last()
         this.panelRegister = page.locator('[class="panel register"]')
+        this.loginText = page.locator('text=Login')
+        this.elementWelcome = page.locator('[id="WelcomeContent"]')
+        this.elementLoginFailed = page.locator('[class="panel failed"]')
     }
 }
