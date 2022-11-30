249db5872e67d6651c5de7072dd842f43ff21ce7
Elena Kogai <kogai.lena@gmail.com>
2022-12-01 01:35:21 +0500

    Переустановила playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -51,7 +51,7 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    /*{
+    {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
@@ -63,7 +63,7 @@ const config: PlaywrightTestConfig = {
       use: {
         ...devices['Desktop Safari'],
       },
-    },*/
+    },
 
     /* Test against mobile viewports. */
     // {
